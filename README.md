d62cc45a44ed306f187652e6f1742931da94f547
osmar <osmar@OSMAR-POSITIVO>
2020-05-05 11:34:16 -0300

    pesquisa por nome

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Sistema de Cadastro de Clientes para Desktop utilizando Arquivo Binário Sequencial.
 
+## Características:<br>
 - Utiliza um arquivo binário manipulado pela classe RandomAcessFile.<br>
 - Os registros são armazenados de forma sequencial.<br>
 - A iteração com o usuário é através de JOptionPane.<br>
@@ -7,14 +8,17 @@
 - Toda iteração com o arquivo binário é tratada diretamente na classe GerenciadorCliente.<br>
 - A pasta src contêm os fontes do projeto.<br>
 - O arquivo de dados é gerado na raiz do projeto.<br>
-- Operações disponíveis:<br>
+
+## Operações disponíveis:<br>
  - Incluir<br>
+ - Incluir com verificação<br>
  - Atualizar<br>
  - Excluir Lógico<br>
  - Excluir Físico<br>
  - Pesquisar Chave<br>
  - Pesquisar Posição<br>
+ - Pesquisar Nome<br>
  - Listar Lógico<br>
  - Listar Físico<br>
  - Informações<br>
- - Zera Arquivo<br>
+ - Zerar Arquivo<br>
--- src/Principal.java
+++ src/Principal.java
@@ -11,6 +11,7 @@ public class Principal {
 
     /**
      * Realiza o preenchimento de um objeto do tipo cliente.
+     *
      * @param mensagem Mensagem inícia antes da leitura.
      * @return Um objeto preenchido.
      */
@@ -30,8 +31,8 @@ public class Principal {
     }
 
     /**
-      * Programa principal
-      */
+     * Programa principal
+     */
     public static void main(String Arg[]) {
         //Classe que gerencia o arquivo de cliente
         GerenciadorCliente gerente = new GerenciadorCliente();
@@ -40,15 +41,17 @@ public class Principal {
         do {
             opcao = Integer.parseInt(JOptionPane.showInputDialog("\t### Arquivo Binário Sequencial ###\n"
                     + " 1 - Incluir \n "
-                    + " 2 - Atualizar \n "
-                    + " 3 - Excluir Lógico\n "
-                    + " 4 - Excluir Físico\n "
-                    + " 5 - Pesquisar Chave\n "
-                    + " 6 - Pesquisar Posição\n "
-                    + " 7 - Listar Lógico\n "
-                    + " 8 - Listar Físico \n "
-                    + " 9 - Informações \n "
-                    + "10 - Zera Arquivo \n "
+                    + " 2 - Incluir com verificação \n "
+                    + " 3 - Atualizar \n "
+                    + " 4 - Excluir Lógico\n "
+                    + " 5 - Excluir Físico\n "
+                    + " 6 - Pesquisar Chave\n "
+                    + " 7 - Pesquisar Posição\n "
+                    + " 8 - Pesquisar Nome\n "
+                    + " 9 - Listar Lógico\n "
+                    + "10 - Listar Físico \n "
+                    + "11 - Informações \n "
+                    + "12 - Zera Arquivo \n "
                     + "99 - Sair\n"
                     + "Digite uma Opção: "));
 
@@ -64,6 +67,31 @@ public class Principal {
                     break;
                 }
                 case 2: {
+                    //Chama o método leitura para retornar um cliente instanciado e preenchido
+                    RegistroCliente cliente = leitura("Cadastro de cliente com verificação do código");
+
+                    // Procura a posição do registro com o código no arquivo
+                    int posicao = gerente.posicaoRegistro(cliente.getCodigo());
+
+                    //Enquanto não for localizado(posição != -1) e o usuário desejar proseguir
+                    while (posicao != -1 && cliente.getCodigo() != -2) {
+                        cliente.setCodigo(Integer.parseInt(JOptionPane.showInputDialog("Este código já existe digite um novo código para o cliente ou -2 para sair:")));
+                        // Procura a posição do registro com o código no arquivo
+                        posicao = gerente.posicaoRegistro(cliente.getCodigo());
+                    }                    
+                    //Se código diferente de -2 realiza a a inclusão
+                    if (cliente.getCodigo() != -2) {
+                        if (gerente.inserirFimArquivo(cliente) == true) {
+                            JOptionPane.showMessageDialog(null, "Registro inserido com sucesso.");
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Registro não foi inserido.");
+                        }
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Registro não foi inserido.");
+                    }
+                    break;
+                }
+                case 3: {
                     //Pergunta qual o código a ser atualizado
                     int codigoAtualizar = Integer.parseInt(JOptionPane.showInputDialog(" Digite o código a ser atualizado: "));
                     //Instancia um novo cliente com os novos dados
@@ -76,7 +104,7 @@ public class Principal {
                     }
                     break;
                 }
-                case 3: {
+                case 4: {
                     //Pergunta qual o código a ser excluído logicamente
                     int codigoExcluir = Integer.parseInt(JOptionPane.showInputDialog(" Digite o codigo a ser excluído: "));
                     //Excluí o registro com o código especificado                    
@@ -87,7 +115,7 @@ public class Principal {
                     }
                     break;
                 }
-                case 4: {
+                case 5: {
                     //Pergunta qual o código a ser excluído fisicamente
                     int codigoExcluir = Integer.parseInt(JOptionPane.showInputDialog(" Digite o codigo a ser excluído: "));
                     //Excluí o registro com o código especificado                    
@@ -98,7 +126,7 @@ public class Principal {
                     }
                     break;
                 }
-                case 5: {
+                case 6: {
                     // Pergunta qual a chave do cliente deve ser procurada no arquivo
                     int chave = Integer.parseInt(JOptionPane.showInputDialog(" Digite o código a ser perquisado: "));
                     // Procura o registro do cliente com a chave no arquivo
@@ -111,7 +139,7 @@ public class Principal {
                     }
                     break;
                 }
-                case 6: {
+                case 7: {
                     // Pergunta qual a chave do cliente deve ser procurada sua posição no arquivo
                     int chave = Integer.parseInt(JOptionPane.showInputDialog(" Digite o código ser procurado a posição: "));
                     // Procura a posição do registro com chave no arquivo
@@ -124,25 +152,38 @@ public class Principal {
                     }
                     break;
                 }
-                case 7: {
+                case 8: {
+                    // Pergunta qual o nome do cliente deve ser procurada no arquivo
+                    String chave = JOptionPane.showInputDialog(" Digite o nome a ser perquisado: ");
+                    // Procura o registro do cliente com a chave no arquivo
+                    RegistroCliente cliente = gerente.pesquisarRegistroNome(chave);
+                    // Se cliente != null encontrou o registro
+                    if (cliente != null) {
+                        JOptionPane.showMessageDialog(null, "Achei o registro \n" + cliente.toString());
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Não Achei o registro com a chave " + chave);
+                    }
+                    break;
+                }
+                case 9: {
                     //Lista logicamente os dados do arquivo. Não inclui chave com -1                    
                     String saida = gerente.listarLogico();
                     JOptionPane.showMessageDialog(null, "Lista Lógico:\n" + saida);
                     break;
                 }
-                case 8: {
+                case 10: {
                     //Lista fisicamente os dados do arquivo
                     String saida = gerente.listarFisico();
                     JOptionPane.showMessageDialog(null, "Lista Físico:\n" + saida);
                     break;
                 }
-                case 9: {
+                case 11: {
                     //Retorna as informações do arquivo
                     String informacoes = gerente.informacoes();
                     JOptionPane.showMessageDialog(null, informacoes);
                     break;
                 }
-                case 10: {
+                case 12: {
                     //Esvazia o arquivo de dados
                     if (gerente.zeraArquivo() == true) {
                         JOptionPane.showMessageDialog(null, "Arquivo zerado com sucesso!");
